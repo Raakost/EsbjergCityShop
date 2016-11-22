87e0cc09daee181da40cf8bcfb0e4cb39ab7e032
Raakost <jeanettesydboege@gmail.com>
2016-11-22 08:59:44 +0100

    initial commit

---
+++ EsbjergCityShop/EsbjergCityShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EsbjergCityShop.MvcApplication" Language="C#" %>
---
+++ EsbjergCityShop/EsbjergCityShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EsbjergCityShop.Startup))]
+namespace EsbjergCityShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
