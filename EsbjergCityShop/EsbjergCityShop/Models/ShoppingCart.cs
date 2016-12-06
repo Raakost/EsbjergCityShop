ea9bb68c0f1120bc38badaa69b49c523a87b9893
Raakost <jeanettesydboege@gmail.com>
2016-12-06 15:48:36 +0100

    ..

---
+++ EsbjergCityShop/EsbjergCityShop/Models/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gateway.Models;
+
+namespace EsbjergCityShop.Models
+{
+    public class ShoppingCart
+    {
+        public List<GiftCard> GiftCards { get; set; }
+    }
+}
